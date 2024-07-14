3bcd10c4e63f27e0a3b52f596155cae93d13306b
fatma-alzahraa4 <Kfatmaalzahraa@gmail.com>
2024-07-14 20:03:57 +0300

    ffff

--- src/services/multerCloudinary.js
+++ src/services/multerCloudinary.js
@@ -3,7 +3,11 @@ import { allowedExtensions } from './../utils/allowedEtensions.js';
 import fs from 'fs';
 import path from 'path';
 import sharp from 'sharp';
+import ffmpegPath from '@ffmpeg-installer/ffmpeg';
 import ffmpeg from 'fluent-ffmpeg';
+
+ffmpeg.setFfmpegPath(ffmpegPath.path);
+
 
 export const multerCloudFunction = (allowedExtensionsArr)=>{
     if(!allowedExtensionsArr){
@@ -23,16 +27,19 @@ export const multerCloudFunction = (allowedExtensionsArr)=>{
 
 
 
-const convertVideoToWebP = async (file) => {
+const convertVideoToWebP = async (res,file) => {
   const inputFilePath = file.path;
-  const outputFilePath = path.join(path.dirname(inputFilePath), `${path.parse(file.originalname).name}.webp`);
-  // console.log(inputFilePath);
-  // console.log(outputFilePath);
-
+  const outputFilePath = path.join(path.dirname(inputFilePath), `${path.parse(file.originalname).name}.webm`);
   return new Promise((resolve, reject) => {
     ffmpeg(inputFilePath)
-      .inputOptions(['-vf', 'scale=640:-1', '-loop 0', '-r 30'])
-      .toFormat('webp')
+      .output(outputFilePath)
+      .videoCodec('libvpx-vp9')
+      .outputOptions([
+        '-vf scale=640:-1',
+        '-b:v 1M',
+        '-r 30',
+        '-an',
+      ])
       .on('end', () => {
         file.path = outputFilePath;
         file.mimetype = 'video/webp';
@@ -41,8 +48,9 @@ const convertVideoToWebP = async (file) => {
       .on('error', (err) => {
         reject(new Error(`FFmpeg conversion failed: ${err.message}`));
       })
-      .save(outputFilePath);
+      .run();
   });
+  
 };
 
 
@@ -70,7 +78,7 @@ export const convertToWebP = async (req, res, next) => {
         await convertImageToWebP(req.file);
       }
       else if (req.file.mimetype.startsWith('video/')) {
-        await convertVideoToWebP(req.file);
+        await convertVideoToWebP(res,req.file);
       }
       // For single video files, just call next as we don't convert videos
     } else if (req.files) {
@@ -86,7 +94,7 @@ export const convertToWebP = async (req, res, next) => {
         }
         else if (file.mimetype.startsWith('video/')) {
           console.log("videooo");
-          await convertVideoToWebP(file);
+          await convertVideoToWebP(res,file);
         }
         // You can add handling for other file types here if needed
       }
