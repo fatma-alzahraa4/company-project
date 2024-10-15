f01df05c2cb5b34379bc65833418e96ada2638da
fatma-alzahraa4 <kfatmaalzahraa@gmail.com>
2024-10-15 14:45:42 +0300

    edit

--- src/modules/websiteAPIs/project/project.controller.js
+++ src/modules/websiteAPIs/project/project.controller.js
@@ -2,62 +2,58 @@ import { projectModel } from "../../../../DB/models/projectModel.js";
 import { paginationFunc } from "../../../utils/pagination.js";
 
 export const getProjects = async (req, res, next) => {
-    const {page,size } = req.query
-    console.log(page,size);
-    
+    const { page, size } = req.query
     const { limit, skip } = paginationFunc({ page, size });
-    console.log(limit,skip);
-    
     const projects = await projectModel.find()
-    .select('-mainImage.public_id -mainImage.customId -updatedAt -progressPercentage -projectFolder -video.customId -video.public_id -__v')
-    .populate([
-        {
-            path:'images',
-            select:'image.secure_url image.alt -projectId'
-        },
-        {
-            path:'categoryId',
-            select:'name'
-        }
-    ]).sort({ date: -1 }) 
-    .limit(limit)
-    .skip(skip);
+        .select('-mainImage.public_id -mainImage.customId -updatedAt -progressPercentage -projectFolder -video.customId -video.public_id -__v')
+        .populate([
+            {
+                path: 'images',
+                select: 'image.secure_url image.alt -projectId'
+            },
+            {
+                path: 'categoryId',
+                select: 'name'
+            }
+        ]).sort({ date: -1 })
     const formattedProjects = projects.map(project => {
         const formattedImages = project.images.map(img => ({
             secure_url: img.image.secure_url,
             alt: img.image.alt
         }));
-    
+
         return {
             ...project.toObject(),  // Convert the Mongoose document to a plain object
             images: formattedImages  // Replace the images array with the formatted one
         };
     });
-    return res.status(200).json({ message: 'Done', projects:formattedProjects })
+    const lastEdit = formattedProjects.slice(skip, skip + limit);
+    const projcetsCount = projects.length
+    return res.status(200).json({ message: 'Done', projects: lastEdit, projcetsCount })
 }
 
 export const getProject = async (req, res, next) => {
     const { projectId } = req.params;
     const project = await projectModel.findById(projectId)
-    .select('-mainImage.public_id -mainImage.customId -updatedAt -progressPercentage -projectFolder -video.customId -video.public_id -__v')
-    .populate([
-        {
-            path:'images',
-            select:'image.secure_url image.alt -projectId'
-        },
-        {
-            path:'categoryId',
-            select:'name'
-        }
-    ])
-        const formattedImages = project.images.map(img => ({
-            secure_url: img.image.secure_url,
-            alt: img.image.alt
-        }));
-    
-       const formattedProject = {
-           ...project.toObject(),  
-           images: formattedImages 
-       }
-    return res.status(200).json({ message: 'Done', project:formattedProject })
+        .select('-mainImage.public_id -mainImage.customId -updatedAt -progressPercentage -projectFolder -video.customId -video.public_id -__v')
+        .populate([
+            {
+                path: 'images',
+                select: 'image.secure_url image.alt -projectId'
+            },
+            {
+                path: 'categoryId',
+                select: 'name'
+            }
+        ])
+    const formattedImages = project.images.map(img => ({
+        secure_url: img.image.secure_url,
+        alt: img.image.alt
+    }));
+
+    const formattedProject = {
+        ...project.toObject(),
+        images: formattedImages
+    }
+    return res.status(200).json({ message: 'Done', project: formattedProject })
 }
